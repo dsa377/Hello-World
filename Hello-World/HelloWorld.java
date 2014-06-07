58a148e918fea49f686bc2dba1f57f45a18e01cd
dsa377 <danielangel@daniels-mbp.hsd1.tx.comcast.net>
2014-06-07 17:59:59 -0500

    first commit
    Hello World Java Program

---
+++ Hello-World/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+/**
+ * Simple Hello World program to test committing to github.
+ * 
+ * @author Daniel Angel 
+ * @version 1.0
+ */
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
